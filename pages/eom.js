78a03f107c2f7351ed0a7bd046cbb4d37640ba67
Selena Siri <selenasiri@gmail.com>
2021-02-09 18:57:37 -0500

    first commit

---
+++ pages/eom.js
@@ -0,0 +1,11 @@
+export const EOM = () => {
+  return(
+    <div className='page-container'>
+      <div>
+        <h1>Employee of the Month</h1>
+      </div>
+    </div>
+  )
+};
+
+export default EOM
