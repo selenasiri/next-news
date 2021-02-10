2abe0f5884fb203072daf7ea3286809729ddf38f
Selena Siri <selenasiri@gmail.com>
2021-02-09 21:34:43 -0500

    employee of the month

--- pages/eom.js
+++ pages/eom.js
@@ -1,11 +1,35 @@
-export const EOM = () => {
+import styles from '../styles/EOM.module.css'
+
+export const EOM = ({employee}) => {
   return(
     <div className='page-container'>
-      <div>
-        <h1>Employee of the Month</h1>
+      <div className={styles.main}>
+        <h1>Employee Of The Month</h1>
+      </div>
+
+      <div className={styles.employeeOfTheMonth}>
+        <h3>{employee.name}</h3>
+        <h6>{employee.position}</h6>
+        <img src={employee.image} />
+        <p>{employee.description}</p>
       </div>
     </div>
   )
 };
+
+// runs asyncronously on the server, then passed as a prop
+export const getServerSideProps = async pageContext => {  
+  const apiResponse = await fetch(
+    'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth'
+  );
+
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee
+    }
+  }
+}
 
 export default EOM
