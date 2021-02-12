232f33b14842718530d5cd5c364619522d1605ec
Selena Siri <selenasiri@gmail.com>
2021-02-11 21:12:28 -0500

    News App

--- components/toolbar.js
+++ components/toolbar.js
@@ -6,7 +6,10 @@ export const Toolbar = () => {
 
   return(
     <div className={styles.main}>
-
+      <div onClick={() => router.push('/')}>Home</div>
+      <div onClick={() => router.push('/feed/1')}>Feed</div>
+      <div onClick={() => router.push('/eom/')}>EOM</div>
+      <div onClick={() => window.location.href = 'https://twitter.com/portexe'}>Twitter</div>
     </div>
   )
 };
