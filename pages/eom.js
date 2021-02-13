89be2d2887a09edd62e6e4c970f3be44029517af
Selena Siri <selenasiri@gmail.com>
2021-02-12 20:10:30 -0500

    Toolbar

--- pages/eom.js
+++ pages/eom.js
@@ -1,8 +1,10 @@
 import styles from '../styles/EOM.module.css'
+import { Toolbar } from '../components/toolbar'
 
 export const EOM = ({employee}) => {
   return(
     <div className='page-container'>
+      <Toolbar />
       <div className={styles.main}>
         <h1>Employee Of The Month</h1>
       </div>
