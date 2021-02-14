1829f4cad65a0f5c92b63f3b41b2b196f273879b
Selena Siri <selenasiri@gmail.com>
2021-02-13 22:22:02 -0500

    newsfeed and css

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,6 +1,44 @@
 // create feed page inside here (dynamic route)
-export const Feed =() => {
-  return (<>Hello World </>);
+import styles from '../../styles/Feed.module.css'
+
+export const Feed = ({ pageNumber, articles }) => {
+  return (
+    <div className={styles.main}> 
+      {articles.map((article, index) => (
+        <div key={index} className={styles.post}>
+          <h1>{article.title}</h1>
+          <p>{article.description}</p>
+          {!!article.urlToImage && <img src={article.urlToImage} />}
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export const getServerSideProps = async pageContext => {
+  const pageNumber = pageContext.query.slug;
+
+  if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
+    return {
+      props: {
+        articles: [],
+        pageNumber: 1,
+      }
+    }
+  }
+
+  const apiResponse = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
+    },
+  );
+
+
+  
+
+};
 
 export default Feed;
