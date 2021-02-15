27903b9618b0bd7f2efbd46cc41fc28ee319002e
Selena Siri <selenasiri@gmail.com>
2021-02-14 23:35:09 -0500

    slug

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,16 +1,36 @@
 import styles from '../../styles/Feed.module.css'
+import { useRouter } from 'next/router';
 import axios from 'axios'
 
 const Feed = ({ pageNumber, articles }) => {
+
+  const router = useRouter();
+
   return (
-    <div className={styles.main}> 
-      {articles.map((article, index) => (
-        <div key={index} className={styles.post}>
-          <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
-          <p>{article.description}</p>
-          {!!article.urlToImage && <img src={article.urlToImage} />}
+    <div className='page-container'>
+      <div className={styles.main}> 
+            {articles.map((article, index) => (
+              <div key={index} className={styles.post}>
+                <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+                <p>{article.description}</p>
+                {!!article.urlToImage && <img src={article.urlToImage} />}
+              </div>
+            ))};
         </div>
-      ))}
+
+        <div className={styles.paginator}>
+          <div 
+            onClick={() => {
+              if (pageNumber > 1) {
+                router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0,0));
+              }
+            }}
+            className={pageNumber === 1 ? styles.disabled : styles.active}>
+              Previous Page
+          </div>
+
+          <div>#{pageNumber}</div>
+        </div>
     </div>
   );
 }
